3bee65e6ea6ce7d42a44958635d2ae9418c6fe9b
user <user@DESKTOP-90F692B>
2023-03-11 19:00:30 +0100

    change absolute signatures path

--- src/main/java/org/jinwalk/readers/JinReader.java
+++ src/main/java/org/jinwalk/readers/JinReader.java
@@ -8,24 +8,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class JinReader {
     private static final Logger logger = LoggerFactory.getLogger(JinReader.class);
-    private FileInputStream file;
-    private DataInputStream data;
-    private List<Signature> signatures;
+    private static final String RESOURCES_SIGNATURES_CSV = "resources/signatures.csv";
 
-    public JinReader(String path) throws Exception {
+    public JinReader(String path) {
 
-        file = new FileInputStream(path);
-        data = new DataInputStream(file);
-        signatures = initializeSignatures();
-        byte[] fileRaw = data.readAllBytes();
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            logger.error(e.getMessage());
+        }
+        List<Signature> signatures = null;
+        byte[] fileRaw = new byte[0];
+        try {
+            assert file != null;
+            try (DataInputStream data = new DataInputStream(file)) {
+                signatures = initializeSignatures();
+                fileRaw = data.readAllBytes();
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        assert signatures != null;
         for (Signature signature : signatures) {
             int index = Bytes.indexOf(fileRaw, signature.getSignature());
             if (index >= 0) {
@@ -37,28 +52,25 @@ public class JinReader {
     }
 
     private List<Signature> initializeSignatures() throws IOException {
-        String path = "D:\\Workspace\\JinWalk\\JinWalk\\resources\\signatures.csv";
-        CSVReader reader = null;
-        try {
-            reader = new CSVReader(new FileReader(path));
+        File signaturesFile = new File(RESOURCES_SIGNATURES_CSV);
+
+        try (CSVReader reader = new CSVReader(new FileReader(signaturesFile))) {
             reader.skip(1);
             List<Signature> signatures = new ArrayList<>();
 
             // read line by line
-            String[] record = null;
+            String[] csvRecord;
 
-            while ((record = reader.readNext()) != null) {
+            while ((csvRecord = reader.readNext()) != null) {
                 Signature signature = new Signature();
-                signature.setSignature(Hex.decodeHex(record[0]));
-                signature.setDescription(record[1]);
+                signature.setSignature(Hex.decodeHex(csvRecord[0]));
+                signature.setDescription(csvRecord[1]);
                 signatures.add(signature);
             }
             return signatures;
         } catch (Exception e) {
             logger.error(e.getMessage());
-        } finally {
-            reader.close();
         }
-        return signatures;
+        return Collections.emptyList();
     }
 }
