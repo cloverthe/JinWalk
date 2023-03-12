406e07f5774ab96ddaf031551f0b71b0746221fc
user <user@DESKTOP-90F692B>
2023-03-12 09:58:43 +0100

    change signature search for multiple occurrences in the same file (recursion)

--- src/main/java/org/jinwalk/readers/JinReader.java
+++ src/main/java/org/jinwalk/readers/JinReader.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -46,11 +47,21 @@ public class JinReader {
         }
         assert signatures != null;
         for (Signature signature : signatures) {
-            int index = Bytes.indexOf(fileRaw, signature.getSignature());
-            if (index >= 0) {
-                logger.info("Found {} at index {}", signature.getDescription(), index);
+            handleSignatureCheck(fileRaw, signature, 0);
+        }
 
+    }
+
+    private static void handleSignatureCheck(byte[] fileRaw, Signature signature, int initialIndex) {
+        int newIndex = Bytes.indexOf(fileRaw, signature.getSignature());
+        if (newIndex != -1) {
+            logger.info("Found {} at index {}", signature.getDescription(), newIndex);
+            if (newIndex != initialIndex) {
+                // need probably to make this variable local, not sure how it works with recursion
+                fileRaw = Arrays.copyOfRange(fileRaw, newIndex, fileRaw.length);
+                handleSignatureCheck(fileRaw, signature, newIndex);
             }
+
         }
 
     }
